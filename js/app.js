48a1e8f8e1487933fadcece283986bbfc0687876
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-24 10:58:36 -0500

    Showing the alert message on the screen

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-// Selectores
-
+// UI
+const container = document.querySelector(".container");
+const contenido = document.querySelector("#contenido");
 const form = document.querySelector("#nueva-cita");
+const contenedorCitas = document.querySelector("#citas");
 
+// Selectores de inputs
 const mascota = document.querySelector("#mascota");
 const propietario = document.querySelector("#propietario");
 const telefono = document.querySelector("#telefono");
@@ -11,19 +14,28 @@ const sintomas = document.querySelector("#sintomas");
 
 // Objeto principal - Información del Usuario
 
-let citaDatos = {};
+let citaDatos = {
+  mascota: "",
+  propietario: "",
+  telefono: "",
+  fecha: "",
+  hora: "",
+  sintomas: "",
+};
 
 // Event listeners
 
 eventListeners();
 
 function eventListeners() {
-  mascota.addEventListener("input", handleInput);
   propietario.addEventListener("input", handleInput);
+  sintomas.addEventListener("input", handleInput);
   telefono.addEventListener("input", handleInput);
+  mascota.addEventListener("input", handleInput);
   fecha.addEventListener("input", handleInput);
   hora.addEventListener("input", handleInput);
-  sintomas.addEventListener("input", handleInput);
+
+  form.addEventListener("submit", handleSubmit);
 }
 
 // Recolector de datos
@@ -31,4 +43,53 @@ function handleInput(e) {
   e.preventDefault();
   citaDatos[e.target.name] = e.target.value;
   console.log(citaDatos);
+}
+
+//Clases principales
+class Citas {
+  constructor() {
+    this.citas = [];
+  }
+}
+class UIcitas {
+  imprimirAlert(mensaje, tipo) {
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add("text-center", "d-block", "col-12", "alert");
+    // evalua el tipo de alerta
+    if (tipo === "error") {
+      divMensaje.classList.add("alert-danger");
+    } else {
+      divMensaje.classList.add("alert-success");
+    }
+    // Mostrar el mensaje;
+    divMensaje.textContent = mensaje;
+    container.insertBefore(divMensaje, contenido);
+    // borrar el mensaje
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+}
+//instanciaciones de las clases para dejarlas disponible a nivel global
+const citas = new Citas();
+const ui = new UIcitas();
+
+// Al crear nueva cita
+
+function handleSubmit(e) {
+  e.preventDefault();
+  // Validar formulario
+  const { propietario, sintomas, telefono, mascota, fecha, hora } = citaDatos;
+  if (
+    propietario == "" ||
+    sintomas === "" ||
+    telefono === "" ||
+    mascota === "" ||
+    fecha === "" ||
+    hora === ""
+  ) {
+    ui.imprimirAlert("Todos los campos debe estar llenos", "error");
+    return;
+  }
+  console.log("validando");
 }
